53ca25edf091982109678ea48b3eff59f6f0c80e
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-20 03:37:38 -0300

    edit instructor ok

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const data = require("./data.json");
-const { age } = require("./utils");
+const { age, date } = require("./utils");
 
 
 
@@ -61,7 +61,51 @@ exports.show = function (req, res){
 };
 
 exports.edit = function (req, res){
-    return res.render("instructors/edit");
+
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id;
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!");
+
+    const instructor = {
+        ... foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render("instructors/edit", { instructor });
 };
 
+exports.put = function (req, res){
+
+    const { id } = req.body;
+    
+    let index = 0;
+
+    const foundInstructor = data.instructors.find(function(instructor, foundIndex){
+
+        if(id == instructor.id){
+            index = foundIndex;
+            return true;
+        };        
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!");
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body, 
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor;
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!");
+
+        return res.redirect(`/instructors/${id}`);
+    });
+}
 
