eab03b9babcfc912e8c355518cb0977c01b68c57
ZMarina Monteiro <zmarina.hm@gmail.com>
2020-07-20 02:02:56 -0300

    create instructor ok

---
+++ instructors.js
@@ -0,0 +1,67 @@
+const fs = require("fs");
+const data = require("./data.json");
+const { age } = require("./utils");
+
+
+
+exports.post = function (req, res){
+    
+    const keys = Object.keys(req.body);
+
+    for (key of keys){
+        if (req.body[key] == "")
+            return res.send("Please, fill all fields!");        
+    }
+
+    let { avatar_url, birth, name, services, gender } = req.body;
+
+    birth = Date.parse(birth);
+    const created_at = Date.now();
+    const id = Number(data.instructors.length + 1);
+
+    data.instructors.push({
+        id,
+        avatar_url,
+        name,        
+        birth,           
+        gender,
+        services,  
+        avatar_url              
+    });
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/instructors")
+
+    })
+
+    //return res.send (req.body);
+};
+
+exports.show = function (req, res){
+   
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found!");
+    
+  
+    const instructor = {
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at)
+    }
+
+    return res.render("instructors/show", { instructor });
+};
+
+exports.edit = function (req, res){
+    return res.render("instructors/edit");
+};
+
+
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require ("express");
 const routes = express.Router();
+const instructors = require ("./instructors")
 
 routes.get("/", function (req, res){
     return res.redirect ("/instructors");
@@ -8,6 +9,16 @@ routes.get("/", function (req, res){
 routes.get("/instructors", function (req, res){
     return res.render ("instructors/index");
 });
+
+routes.get("/instructors/create", function (req, res){
+    return res.render ("instructors/create")
+})
+
+routes.get("/instructors/:id", instructors.show);
+
+routes.get("/instructors/:id/edit", instructors.edit);
+
+routes.post("/instructors", instructors.post);
 
 routes.get("/students", function (req, res){
     return res.send ("students");
